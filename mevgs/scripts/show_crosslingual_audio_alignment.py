caca6eb99567792fe2270cb417fea394e07a4723
Dan Oneata <dan.oneata@gmail.com>
2024-10-25 23:24:31 +0300

    FIX Word order and refactor

--- mevgs/scripts/show_crosslingual_audio_alignment.py
+++ mevgs/scripts/show_crosslingual_audio_alignment.py
@@ -7,9 +7,13 @@ import streamlit as st
 import torch
 
 from functools import partial
+from itertools import combinations, product
 from matplotlib import pyplot as plt
 from procrustes import orthogonal
+from scipy.stats import spearmanr, kendalltau
+
 from sklearn import manifold
+from sklearn.decomposition import PCA
 
 from adjustText import adjust_text
 from toolz import first
@@ -22,6 +26,11 @@ from mevgs.scripts.show_model_comparison import (
 )
 
 
+LANGS_LONG = ["english", "french", "dutch"]
+DATASET = MEDataset("test", LANGS_LONG)  # type: ignore
+WORDS_SEEN = sorted(DATASET.words_seen)
+
+
 def aggregate_by_word(emb, meta, lang=None):
     def aggregate1(emb, meta, word):
         idxs = [
@@ -30,7 +39,8 @@ def aggregate_by_word(emb, meta, lang=None):
             if m["word-en"] == word
             and implies(lang is not None, m["lang"] == LANG_SHORT_TO_LONG[lang])
         ]
-        return emb[idxs].mean(0)
+        emb = emb[idxs].mean(0)
+        return emb / np.linalg.norm(emb)
 
     return np.vstack([aggregate1(emb, meta, word) for word in WORDS_SEEN])
 
@@ -97,11 +107,10 @@ def translate(word_en, lang):
     return entry[lang]
 
 
-def show_tsne(embs, data, langs, ax):
-    tsne = manifold.TSNE(n_components=2, init="pca", random_state=0)
-    embs_tsne = tsne.fit_transform(embs)
-
-    df = pd.DataFrame(embs_tsne, columns=["x", "y"])
+def show_2d(embs_2d, data, langs, ax):
+    # tsne = manifold.TSNE(n_components=2, init="pca", random_state=0)
+    # embs_tsne = tsne.fit_transform(embs)
+    df = pd.DataFrame(embs_2d, columns=["x", "y"])
     df["word-en"] = [d["word-en"] for d in data]
     df["lang"] = [d["lang"] for d in data]
     df["word"] = [translate(d["word-en"], d["lang"]) for d in data]
@@ -124,11 +133,6 @@ def show_tsne(embs, data, langs, ax):
     )
     ax.set_title("-".join(langs))
     add_texts(ax, df)
-
-
-LANGS_LONG = ["english", "french", "dutch"]
-DATASET = MEDataset("test", LANGS_LONG)  # type: ignore
-WORDS_SEEN = set(DATASET.words_seen)
 
 
 def load_data_and_embs(langs, size, variant):
@@ -160,6 +164,86 @@ def load_data_and_embs(langs, size, variant):
     return audio_data, embs
 
 
+def show_rsm(embs, data, lang, ax):
+    emb_agg = aggregate_by_word(embs, data, lang)
+    emb_agg_norm = emb_agg / np.linalg.norm(emb_agg, axis=1)[:, None]
+    sims = emb_agg_norm @ emb_agg_norm.T
+    df = pd.DataFrame(data=sims, index=WORDS_SEEN, columns=WORDS_SEEN)
+    mask = np.triu(sims)
+    sns.heatmap(
+        df,
+        square=True,
+        annot=True,
+        annot_kws={"size": 7},
+        fmt=".1f",
+        cbar=False,
+        vmin=0.5,
+        vmax=1.0,
+        mask=mask,
+        ax=ax,
+    )
+    idxs = np.triu_indices_from(sims, k=1)
+    return sims[idxs].flatten()
+
+
+def show_rms_all(embs1, embs2, embs3, data1, data2, data3, lang1, lang2):
+    fig, axs = plt.subplots(2, 2, figsize=(10, 10), sharex=True, sharey=True)
+
+    rsm11 = show_rsm(embs1, data1, lang1, axs[0, 0])
+    rsm31 = show_rsm(embs3, data3, lang1, axs[0, 1])
+    rsm22 = show_rsm(embs2, data2, lang2, axs[1, 0])
+    rsm32 = show_rsm(embs3, data3, lang2, axs[1, 1])
+
+    axs[0, 0].set_ylabel(lang1)
+    axs[1, 0].set_ylabel(lang2)
+    axs[0, 0].set_title("monolingual")
+    axs[0, 1].set_title("bilingual")
+
+    fig.tight_layout()
+
+    col1, col2 = st.columns([1, 1])
+
+    col1.markdown("## Similarity matrices")
+    col1.pyplot(fig)
+
+    rsms = {
+        f"mono/{lang1}": rsm11,
+        f"mono/{lang2}": rsm22,
+        f"bi/{lang1}": rsm31,
+        f"bi/{lang2}": rsm32,
+    }
+
+    rsa = [
+        {
+            "src": src,
+            "tgt": tgt,
+            "value": spearmanr(rsms[src], rsms[tgt]).correlation,
+            # "value": kendalltau(rsms[src], rsms[tgt]).correlation,
+        }
+        # for src, tgt in list(combinations(rsms.keys(), 2))
+        for src, tgt in product(rsms.keys(), rsms.keys())
+    ]
+
+    df = pd.DataFrame(rsms)
+    fig = sns.pairplot(df)
+
+    n = len(rsms)
+    for i, j in product(range(n), range(n)):
+        src = fig.x_vars[i]
+        tgt = fig.y_vars[j]
+        value = next(
+            item["value"] for item in rsa if item["src"] == src and item["tgt"] == tgt
+        )
+        fig.axes[i, j].set_title(f"ρ = {value:.2f}")
+
+    fig.tight_layout()
+
+    col2.markdown("## Correlation between similarity matrices")
+    col2.pyplot(fig)
+
+    st.markdown("---")
+
+
 def do(langs, size, seed):
     VARIANTS = "abcde"
     variant = VARIANTS[seed]
@@ -171,14 +255,28 @@ def do(langs, size, seed):
     data2, embs2 = load_data_and_embs_1([lang2])
     data3, embs3 = load_data_and_embs_1(langs)
 
-    # embs1, error1 = align(embs1, embs3, data1, data3, lang1)
-    # embs2, error2 = align(embs2, embs3, data2, data3, lang2)
+    embs1, error1 = align(embs1, embs3, data1, data3, lang1)
+    embs2, error2 = align(embs2, embs3, data2, data3, lang2)
+
+    show_rms_all(embs1, embs2, embs3, data1, data2, data3, lang1, lang2)
+
+    # proj = manifold.Isomap(n_components=2)
+    proj = PCA(n_components=2)
+    proj.fit(embs3)
+
+    embs1_2d = proj.transform(embs1)
+    embs2_2d = proj.transform(embs2)
+    embs3_2d = proj.transform(embs3)
 
     fig, axs = plt.subplots(1, 3, figsize=(15, 5), sharex=True, sharey=True)
-    show_tsne(embs1, data1, [lang1], axs[0])
-    show_tsne(embs3, data3, langs, axs[1])
-    show_tsne(embs2, data2, [lang2], axs[2])
+    show_2d(embs1_2d, data1, [lang1], axs[0])
+    show_2d(embs3_2d, data3, langs, axs[1])
+    show_2d(embs2_2d, data2, [lang2], axs[2])
+    st.markdown("## Low-dimensional projections")
     st.pyplot(fig)
+    st.markdown("---")
+
+    # show_rms_all(embs1_2d, embs2_2d, embs3_2d, data1, data2, data3, lang1, lang2)
 
 
 if __name__ == "__main__":
