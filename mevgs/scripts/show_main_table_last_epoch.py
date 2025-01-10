1d6c9a377097d121b576097941b24afeed30772d
Dan Oneata <dan.oneata@gmail.com>
2025-01-10 10:09:46 +0200

    Refactor

--- mevgs/scripts/show_main_table_last_epoch.py
+++ mevgs/scripts/show_main_table_last_epoch.py
@@ -1,5 +1,7 @@
 import pdb
 import pandas as pd
+
+from functools import partial
 from itertools import combinations
 
 from tbparse import SummaryReader
@@ -13,7 +15,7 @@ LANG_SHORT_TO_LONG = {
 }
 
 
-def get_result(train_langs, has_links, model_size, test_lang):
+def get_results_seed(train_langs, has_links, model_size, test_lang, seed):
     model_name = "{}_links-{}_size-{}".format(
         "-".join(train_langs),
         has_links,
@@ -22,37 +24,45 @@ def get_result(train_langs, has_links, model_size, test_lang):
 
     test_lang_long = LANG_SHORT_TO_LONG[test_lang]
 
-    def get1(seed):
-        folder = NAMES[model_name].format(seed)
-        path = f"output/{folder}"
-        results = SummaryReader(path)
-        scalars = results.scalars
-        col1 = "valid/loss"
+    folder = NAMES[model_name].format(seed)
+    path = f"output/{folder}"
+    results = SummaryReader(path)
+    scalars = results.scalars
+    col1 = "valid/loss"
 
-        # print(scalars["tag"].unique())
+    # print(scalars["tag"].unique())
 
-        col_nf = f"test/{test_lang_long}/accuracy-novel-familiar"
-        if col_nf not in scalars["tag"].unique():
-            col_nf = f"test/accuracy-novel-familiar"
+    col_nf = f"test/{test_lang_long}/accuracy-novel-familiar"
+    if col_nf not in scalars["tag"].unique():
+        col_nf = f"test/accuracy-novel-familiar"
 
-        col_ff = f"test/{test_lang_long}/accuracy-familiar-familiar"
-        if col_ff not in scalars["tag"].unique():
-            col_ff = f"test/accuracy-familiar-familiar"
+    col_ff = f"test/{test_lang_long}/accuracy-familiar-familiar"
+    if col_ff not in scalars["tag"].unique():
+        col_ff = f"test/accuracy-familiar-familiar"
 
-        df = scalars[scalars["tag"].isin([col1, col_nf, col_ff])]
-        df = df.pivot(index="step", columns="tag", values="value")
-        best_epoch = df[col1].idxmin()
+    df = scalars[scalars["tag"].isin([col1, col_nf, col_ff])]
+    df = df.pivot(index="step", columns="tag", values="value")
+    best_epoch = df[col1].idxmin()
 
-        return {
-            "epoch-best": best_epoch,
-            "NF": 100 * df.loc[best_epoch, col_nf],
-            "NF-best": 100 * df[col_nf].max(),
-            "NF-last": 100 * df.loc[24, col_nf],
-            "FF": 100 * df.loc[best_epoch, col_ff],
-            "FF-last": 100 * df.loc[24, col_ff],
-        }
+    return {
+        "epoch-best": best_epoch,
+        "NF": 100 * df.loc[best_epoch, col_nf],
+        "NF-best": 100 * df[col_nf].max(),
+        "NF-last": 100 * df.loc[24, col_nf],
+        "FF": 100 * df.loc[best_epoch, col_ff],
+        "FF-last": 100 * df.loc[24, col_ff],
+    }
 
-    df = [{"seed": s, **get1(s)} for s in "abcde"]
+
+def get_result(train_langs, has_links, model_size, test_lang):
+    get_results_seed_1 = partial(
+        get_results_seed,
+        train_langs,
+        has_links,
+        model_size,
+        test_lang,
+    )
+    df = [{"seed": s, **get_results_seed_1(s)} for s in "abcde"]
     # df = [{"seed": s, **get1(s)} for s in "abcdefghij"]
     df = pd.DataFrame(df)
 
@@ -62,7 +72,7 @@ def get_result(train_langs, has_links, model_size, test_lang):
     # ff_value = "{:.1f}±{:.1f}".format(df[col_val].mean(), df[col_val].std())
 
     print(".", end="")
-    
+
     return {
         "train-langs": train_langs,
         "has-links": has_links,
@@ -78,7 +88,7 @@ if __name__ == "__main__":
     TRAIN_LANGS = list(LANG_SHORT_TO_LONG.keys())
 
     def get_train_langs_combinations(test_lang):
-        return [(test_lang, )]  + [
+        return [(test_lang,)] + [
             tuple(sorted([test_lang, other]))
             for other in TRAIN_LANGS
             if other != test_lang
@@ -87,7 +97,7 @@ if __name__ == "__main__":
     results = [
         get_result(train_langs, "no", model_size, test_lang)
         # for train_langs in [("fr", ), ("fr", "nl")]
-        for train_langs in [("nl", ), ("en", "nl"), ("fr", "nl")]
+        for train_langs in [("nl",), ("en", "nl"), ("fr", "nl")]
         for test_lang in train_langs
         # for test_lang in TRAIN_LANGS
         # for train_langs in get_train_langs_combinations(test_lang)
