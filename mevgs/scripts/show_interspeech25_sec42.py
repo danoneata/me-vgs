5abdf3b59efc004ed778d1a9a5632061010ab8c2
Dan Oneata <dan.oneata@gmail.com>
2025-01-10 10:10:30 +0200

    Images for the paper

---
+++ mevgs/scripts/show_interspeech25_sec42.py
@@ -0,0 +1,611 @@
+import pdb
+
+from functools import partial
+from itertools import combinations
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import streamlit as st
+
+from matplotlib import pyplot as plt
+from matplotlib.gridspec import GridSpec
+
+from sklearn.neighbors import NearestCentroid
+from sklearn.metrics import classification_report
+from sklearn.manifold import TSNE, Isomap
+from sklearn.decomposition import PCA
+
+from mevgs.scripts.show_explain_me import (
+    load_data_and_embs_all,
+    WORDS_SEEN,
+    WORDS_UNSEEN,
+    LANG_SHORT_TO_LONG,
+)
+from mevgs.scripts.show_crosslingual_audio_alignment import (
+    add_texts,
+    align,
+    translate,
+)
+
+sns.set(context="poster", style="white", font="Arial")
+
+LANG_LONG_TO_SHORT = {v: k for k, v in LANG_SHORT_TO_LONG.items()}
+
+
+def filter(data, embs, to_keep):
+    idxs = [i for i, datum in enumerate(data) if to_keep(datum)]
+    data1 = [data[i] for i in idxs]
+    embs1 = embs[idxs]
+    return data1, embs1
+
+
+def clear_axes(ax):
+    ax.set_xlabel("")
+    ax.set_ylabel("")
+    ax.set_xticklabels([])
+    ax.set_yticklabels([])
+
+
+def show_2d(data, embs_2d, modality, langs, ax):
+    def get_title(langs):
+        prefix = "Monolingual" if len(langs) == 1 else "Bilingual"
+        langs = [LANG_SHORT_TO_LONG[lang].capitalize() for lang in langs]
+        return "{}: {}".format(prefix, ", ".join(langs))
+
+    df = pd.DataFrame(embs_2d, columns=["x", "y"])
+    df["word-en"] = [d["word-en"] for d in data]
+
+    if modality == "audio":
+        df["lang"] = [d["lang"] for d in data]
+        df["word"] = [translate(d["word-en"], d["lang"]) for d in data]
+        kwargs = {
+            "style": "lang",
+            "markers": {
+                "english": "o",
+                "french": "X",
+                "dutch": "s",
+            },
+        }
+    else:
+        kwargs = {
+            "marker": "P",
+        }
+
+    sns.scatterplot(
+        data=df,
+        x="x",
+        y="y",
+        hue="word-en",
+        legend=False,
+        ax=ax,
+        **kwargs,
+    )
+
+    if modality == "audio":
+        add_texts(ax, df, fontsize=13)
+
+    ax.set_title(get_title(langs))
+
+    return ax
+
+
+def do1(train_langs, size, seed):
+    modalities = ["audio", "image"]
+    langs_combinations = [
+        (train_langs[0],),
+        (train_langs[1],),
+        train_langs,
+    ]
+
+    words = set(WORDS_SEEN)
+
+    def to_keep(datum, modality, langs):
+        cond1 = datum["word-en"] in words
+        cond2 = modality == "image" or LANG_LONG_TO_SHORT[datum["lang"]] in langs
+        return cond1 and cond2
+
+    data_embs = {
+        (modality, langs): filter(
+            *load_data_and_embs_all(
+                modality,
+                langs,
+                size,
+                seed,
+            ),
+            partial(to_keep, modality=modality, langs=langs),
+        )
+        for modality in modalities
+        for langs in langs_combinations
+    }
+
+    anchor = ("audio", train_langs)
+
+    def align1(k):
+        data1, embs1 = data_embs[k]
+        data2, embs2 = data_embs[anchor]
+        embs, error = align(embs1, embs2, data1, data2, None)
+        print(k, error)
+        return embs
+
+    embs_aligned = {k: align1(k) for k in data_embs}
+
+    anchor_embs = data_embs[anchor][1]
+    proj = PCA(n_components=2)
+    proj.fit(anchor_embs)
+
+    # def project2d(embs):
+    #     proj = PCA(n_components=2)
+    #     return proj.fit_transform(embs)
+    #     # proj = TSNE(n_components=2, random_state=0)
+    #     # return proj.fit_transform(embs)
+
+    # embs = {k: project2d(v[1]) for k, v in data_embs.items()}
+
+    # def align2(k):
+    #     data1, _ = data_embs[k]
+    #     data2, _ = data_embs[anchor]
+    #     embs1, error = align(embs[k], embs[anchor], data1, data2, None)
+    #     st.write(k, error)
+    #     return embs1
+
+    # embs_aligned = {k: align2(k) for k in data_embs}
+    # # embs_aligned = embs
+
+    S = 5
+    nrows = 2
+    ncols = 3
+    sns.set(context="poster", style="white", font="Arial")
+    fig, axs = plt.subplots(
+        nrows,
+        ncols,
+        figsize=(S * ncols, S * nrows),
+        sharex=True,
+        sharey=True,
+    )
+    for r, modality in enumerate(modalities):
+        for c, langs in enumerate(langs_combinations):
+            data, _ = data_embs[(modality, langs)]
+            embs = embs_aligned[(modality, langs)]
+
+            ax = axs[r, c]
+            # ax = show_2d(data, embs, modality, langs, ax)
+            ax = show_2d(data, proj.transform(embs), modality, langs, ax)
+
+            ax.set_xlabel("")
+            ax.set_xticklabels([])
+            ax.set_yticklabels([])
+
+            if c == 0:
+                ax.set_ylabel("{} embeddings".format(modality.capitalize()))
+
+    fig.set_tight_layout(True)
+    fig.savefig("output/interspeech25/embeddings-monolingual-vs-bilingual.pdf")
+    st.pyplot(fig)
+
+
+def do2(train_langs, size, seed):
+    def to_keep(datum, modality, langs):
+        return modality == "image" or LANG_LONG_TO_SHORT[datum["lang"]] in langs
+
+    data_audio, embs_audio = filter(
+        *load_data_and_embs_all("audio", train_langs, size, seed),
+        partial(to_keep, modality="audio", langs=train_langs),
+    )
+    data_image, embs_image = filter(
+        *load_data_and_embs_all("image", train_langs, size, seed),
+        partial(to_keep, modality="image", langs=train_langs),
+    )
+
+    for datum in data_audio:
+        datum["modality"] = "audio"
+        datum["is-seen"] = datum["word-en"] in WORDS_SEEN
+        datum["type"] = datum["lang"]
+
+    for datum in data_image:
+        datum["modality"] = "image"
+        datum["is-seen"] = datum["word-en"] in WORDS_SEEN
+        datum["type"] = "image"
+
+    data = data_audio + data_image
+    embs = np.concatenate([embs_audio, embs_image], axis=0)
+
+    proj = PCA(n_components=2, random_state=0)
+    embs_2d = proj.fit_transform(embs)
+
+    df = pd.DataFrame(data)
+    df["x"] = embs_2d[:, 0]
+    df["y"] = embs_2d[:, 1]
+
+    fig = plt.figure(figsize=(12, 12))
+    gs = GridSpec(2, 2, fig)
+    ax1 = fig.add_subplot(gs[0, :])
+    ax2 = fig.add_subplot(gs[1, 0])
+    ax3 = fig.add_subplot(gs[1, 1])
+
+    markers = {
+        "english": "o",
+        "french": "X",
+        "dutch": "s",
+        "image": "P",
+    }
+
+    # st.write(df)
+
+    idxs = ~df["is-seen"]
+    sns.scatterplot(
+        data=df[idxs],
+        x="x",
+        y="y",
+        color="gray",
+        style="type",
+        legend=False,
+        alpha=0.5,
+        markers=markers,
+        ax=ax1,
+    )
+
+    idxs = df["is-seen"]
+    sns.scatterplot(
+        data=df[idxs],
+        x="x",
+        y="y",
+        hue="word-en",
+        style="type",
+        legend=False,
+        markers=markers,
+        ax=ax1,
+    )
+
+    clear_axes(ax1)
+    ax1.set_title("Audio and image embeddings")
+
+    # centroids = df.groupby(["modality", "word-en"])["x", "y"].mean()
+    # for word in WORDS_SEEN:
+    #     audio_centroid = centroids.loc[("audio", word)]
+    #     image_centroid = centroids.loc[("image", word)]
+    #     ax.plot(
+    #         [audio_centroid["x"], image_centroid["x"]],
+    #         [audio_centroid["y"], image_centroid["y"]],
+    #         "k--",
+    #         c="gray",
+    #         alpha=0.5,
+    #         linewidth=1,
+    #     )
+
+    # sns.move_legend(ax1, "upper left", bbox_to_anchor=(1, 1))
+    def show_2d(data, embs_2d, modality, langs, ax):
+        df = pd.DataFrame(embs_2d, columns=["x", "y"])
+        df["word-en"] = [d["word-en"] for d in data]
+
+        if modality == "audio":
+            df["lang"] = [d["lang"] for d in data]
+            df["word"] = [translate(d["word-en"], d["lang"]) for d in data]
+            kwargs = {
+                "style": "lang",
+                "markers": {
+                    "english": "o",
+                    "french": "X",
+                    "dutch": "s",
+                },
+            }
+        else:
+            kwargs = {
+                "marker": "P",
+            }
+
+        idxs = df["word-en"].isin(WORDS_SEEN)
+
+        sns.scatterplot(
+            data=df[~idxs],
+            x="x",
+            y="y",
+            color="gray",
+            alpha=0.5,
+            legend=False,
+            ax=ax,
+            **kwargs,
+        )
+
+        sns.scatterplot(
+            data=df[idxs],
+            x="x",
+            y="y",
+            hue="word-en",
+            legend=False,
+            ax=ax,
+            **kwargs,
+        )
+
+        if modality == "audio":
+            add_texts(ax, df[idxs], fontsize=16)
+        else:
+            df2 = df.copy()
+            df2["word"] = df2["word-en"]
+            add_texts(ax, df2[idxs], fontsize=16, key="word")
+
+        ax.set_title("{} embeddings".format(modality.capitalize()))
+        clear_axes(ax)
+
+    proj = TSNE(n_components=2, random_state=0)
+    # proj = PCA(n_components=2, random_state=0)
+
+    show_2d(data_audio, proj.fit_transform(embs_audio), "audio", train_langs, ax2)
+    show_2d(data_image, proj.fit_transform(embs_image), "image", train_langs, ax3)
+
+    fig.set_tight_layout(True)
+    st.pyplot(fig)
+    fig.savefig("output/interspeech25/embeddings-audio-vs-image.pdf")
+
+
+def evaluate_translation():
+    size = "md"
+
+    def l2_normalize(X):
+        return X / np.linalg.norm(X, axis=1)[:, None]
+
+    def get_X_y(data, embs, lang):
+        lang_long = LANG_SHORT_TO_LONG[lang]
+        idxs = [i for i, datum in enumerate(data) if datum["lang"] == lang_long]
+        X = embs[idxs]
+        y = [data[i]["word-en"] for i in idxs]
+        return X, y
+
+    def eval1(data, embs, lang_src, lang_tgt):
+        X_tr, y_tr = get_X_y(data, embs, lang_src)
+        X_te, y_te = get_X_y(data, embs, lang_tgt)
+        ncm = NearestCentroid()
+        ncm.fit(X_tr, y_tr)
+        ncm.centroids_ = l2_normalize(ncm.centroids_)
+        y_pr = ncm.predict(X_te)
+        print(lang_src, lang_tgt)
+        print(classification_report(y_te, y_pr))
+        print()
+        return 100 * ncm.score(X_te, y_te)
+
+    def evaluate_model(train_langs, seed, type_):
+        words = {
+            "seen": set(WORDS_SEEN),
+            "unseen": set(WORDS_UNSEEN),
+        }
+
+        def to_keep(datum, modality, langs):
+            cond1 = datum["word-en"] in words[type_]
+            cond2 = modality == "image" or LANG_LONG_TO_SHORT[datum["lang"]] in langs
+            return cond1 and cond2
+
+        data, embs = filter(
+            *load_data_and_embs_all("audio", train_langs, size, seed),
+            partial(to_keep, modality="audio", langs=train_langs),
+        )
+
+        return [
+            {
+                "src": train_langs[0],
+                "tgt": train_langs[1],
+                "seed": seed,
+                "type_": type_,
+                "accuracy": eval1(data, embs, train_langs[0], train_langs[1]),
+            },
+            {
+                "src": train_langs[1],
+                "tgt": train_langs[0],
+                "seed": seed,
+                "type_": type_,
+                "accuracy": eval1(data, embs, train_langs[1], train_langs[0]),
+            },
+        ]
+
+    results = [
+        r
+        for train_langs in [["en", "fr"], ["en", "nl"], ["fr", "nl"]]
+        for seed in "abcde"
+        for t in ["seen", "unseen"]
+        for r in evaluate_model(train_langs, seed, t)
+    ]
+    df = pd.DataFrame(results)
+    df = (
+        df.groupby(["src", "tgt", "type_"])["accuracy"]
+        .agg(["mean", "std"])
+        .reset_index()
+    )
+    df["accuracy"] = (
+        df["mean"].round(1).astype(str) + "±" + (2 * df["std"]).round(1).astype(str)
+    )
+    df = df.drop(columns=["mean", "std"])
+    df = df.pivot(index=["src", "tgt"], columns="type_")
+    print(df.to_latex())
+    st.write(df)
+
+
+def quantify_variance_comparison():
+    WORDS = {
+        "seen": set(WORDS_SEEN),
+        "unseen": set(WORDS_UNSEEN),
+    }
+
+    size = "md"
+
+    def select_by_word(data, embs, word):
+        idxs = [i for i, datum in enumerate(data) if datum["word-en"] == word]
+        return embs[idxs]
+
+    def compute_var(embs):
+        C = np.cov(embs.T)
+        return np.trace(C)
+
+    def compute1(train_langs, modality, seed, test_langs_idxs):
+        def to_keep(datum, modality, langs):
+            return modality == "image" or LANG_LONG_TO_SHORT[datum["lang"]] in langs
+
+        test_langs = [train_langs[i] for i in test_langs_idxs]
+        data, embs = filter(
+            *load_data_and_embs_all(modality, train_langs, size, seed),
+            partial(to_keep, modality=modality, langs=test_langs),
+        )
+
+        def fmt(word, words_type):
+            if words_type == "seen":
+                return word
+            else:
+                return word + "'"
+
+        return [
+            {
+                "word-type": words_type,
+                "word": fmt(word, words_type),
+                "variance": compute_var(select_by_word(data, embs, word)),
+            }
+            for words_type in ["seen", "unseen"]
+            for word in sorted(WORDS[words_type])
+        ]
+
+    modality = "audio"
+    results = [
+        {
+            "train-langs": "-".join(train_langs),
+            "test-langs-indices": "+".join(map(str, test_langs_idxs)),
+            "seed": seed,
+            **result,
+        }
+        for train_langs in sorted(combinations(["en", "fr", "nl"], 2))
+        for test_langs_idxs in [[0], [1], [0, 1]]
+        for seed in "abcde"
+        for result in compute1(train_langs, modality, seed, test_langs_idxs)
+    ]
+
+    df = pd.DataFrame(results)
+    st.write(df)
+
+    sns.set(style="whitegrid", font="Arial", context="poster")
+
+    fig = sns.catplot(
+        df,
+        x="variance",
+        y="word",
+        hue="test-langs-indices",
+        col="train-langs",
+        height=11,
+    )
+    st.pyplot(fig)
+
+    fig = sns.catplot(
+        df,
+        x="variance",
+        y="word-type",
+        hue="test-langs-indices",
+        col="train-langs",
+        kind="box",
+    )
+    st.pyplot(fig)
+
+
+def quantify_variance():
+    WORDS = {
+        "seen": set(WORDS_SEEN),
+        "unseen": set(WORDS_UNSEEN),
+    }
+
+    train_langs = ["en", "nl"]
+    size = "md"
+
+    def select_by_word(data, embs, word):
+        idxs = [i for i, datum in enumerate(data) if datum["word-en"] == word]
+        return embs[idxs]
+
+    def compute_var(embs):
+        C = np.cov(embs.T)
+        return np.trace(C)
+
+    def compute1(modality, words_type, seed, var_type):
+        def to_keep(datum, modality, langs):
+            cond1 = datum["word-en"] in WORDS[words_type]
+            cond2 = modality == "image" or LANG_LONG_TO_SHORT[datum["lang"]] in langs
+            return cond1 and cond2
+
+        data, embs = filter(
+            *load_data_and_embs_all(modality, train_langs, size, seed),
+            partial(to_keep, modality=modality, langs=train_langs),
+        )
+
+        if var_type == "full":
+            return compute_var(embs)
+        elif var_type == "concept":
+            vars = [
+                compute_var(select_by_word(data, embs, word))
+                for word in WORDS[words_type]
+            ]
+            return np.mean(vars)
+        else:
+            raise ValueError("Invalid var_type")
+
+    results = [
+        {
+            "modality": modality,
+            "words_type": words_type,
+            "seed": seed,
+            "value": compute1(modality, words_type, seed, var_type),
+        }
+        for modality in ["audio", "image"]
+        for words_type in ["seen", "unseen"]
+        for seed in "abcde"
+        for var_type in ["full", "concept"]
+    ]
+
+    df = pd.DataFrame(results)
+    st.write(df)
+
+    df = df.replace(
+        {
+            "modality": {"audio": "Audio", "image": "Image"},
+            "words_type": {"seen": "Familiar", "unseen": "Novel"},
+            "var_type": {"full": "Full", "concept": "Within concept"},
+        }
+    )
+
+    sns.set(style="whitegrid", font="Arial", context="poster")
+    fig = sns.catplot(
+        df,
+        x="modality",
+        y="value",
+        hue="words_type",
+        col="var_type",
+        kind="bar",
+        errorbar="sd",
+        # height=8,
+        # aspect=0.6,
+        # legend_out=False,
+    )
+
+    fig._legend.set_title("")
+
+    for k, ax in fig.axes_dict.items():
+        ax.set_title(k)
+        ax.set_ylabel("Variance")
+        ax.set_xlabel("")
+
+    st.pyplot(fig)
+    fig.savefig("output/interspeech25/variance.pdf")
+
+
+
+if __name__ == "__main__":
+    st.set_page_config(layout="wide")
+    # evaluate_translation()
+    # quantify_variance()
+    quantify_variance_comparison()
+
+    # with st.sidebar:
+    #     train_langs = st.multiselect(
+    #         "Languages:",
+    #         ["en", "fr", "nl"],
+    #         # default=["en", "nl"],
+    #         default=["en", "fr"],
+    #     )
+    #     size = st.selectbox("Size:", ["sm", "md", "lg"], index=1)
+    #     seed = st.selectbox("Seed:", "abcde", index=2)
+
+    # if len(train_langs) == 0:
+    #     st.error("Select at least one language.")
+    # else:
+    #     do1(tuple(train_langs), size, seed)
